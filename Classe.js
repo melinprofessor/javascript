e415c6eb74323e97b614fd9d6d327a66f8e68dcd
Vinicius Melin <melinprofessor@gmail.com>
2019-05-09 16:35:34 -0300

    herança

--- Classe.js
+++ Classe.js
@@ -7,6 +7,8 @@ class Pessoa {
         console.log(`Nome: ${this.nome}`, `Idade: ${this.idade}`, `CPF: ${this.cpf}`, `SEXO: ${this.sexo}`);
     }
 
+    // Metodo não estático
+    // Com retorno void
     compararSexo() {
         if (this.sexo == 'M'.toUpperCase()) {
             console.log(` ${this.nome} é MASCULINO`);
@@ -16,6 +18,8 @@ class Pessoa {
         }
     }
 
+    // Metodo não estático
+    // Com retorno
     maiorIdade() {
         if (this.idade >= 18) {
             return true;
@@ -23,13 +27,52 @@ class Pessoa {
             return false;
         }
     }
+
+    //metodo estático -> static
+    // com retorno void
+    static exemplo(sexo) {
+        // MASCULINO
+        // FEMININO
+        console.log(sexo)
+        if ( sexo == 'M') {
+            console.log('MASCULINO');
+        } else {
+            console.log('FEMININO');
+        }
+        console.log('Este é um metodo estático.')
+    }
+
+    //metodo estático -> static
+    // com retorno booleano
+    static verificaNome(nome) {
+        if(nome.toUpperCase() == nome) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
+
+// Estou instanciando um novo objeto(classe)
+
 const vinicius = new Pessoa('444.444.444-05', 'Vinicius', 24, 'M');
+
+// Estou acessando um metódo não estático
+// Que o retorno void
 vinicius.compararSexo();
+
+// Este é metodo estático
+// Não precisa instaciar
+// Verifica se nome é maisculo
+Pessoa.exemplo(vinicius.sexo);
+if(!Pessoa.verificaNome(vinicius.nome)) {
+    vinicius.nome = vinicius.nome.toUpperCase();
+}
 if (vinicius.maiorIdade()) {
     console.log(`${vinicius.nome} É MAIOR DE IDADE!`);
 } else {
     console.log(`${vinicius.nome} É MENOR DE IDADE!`);
 }
 
+console.info(Pessoa);
